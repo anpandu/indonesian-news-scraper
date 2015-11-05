8ad8c94ff6a1297aa8557048b0508a5dabb8fb94
anpandu <anpandumail@gmail.com>
2015-11-05 13:34:11 +0700

    v1.2.0

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var assert = require('assert');
 var index = require('../lib');
 
-describe('news-scrapper', function () {
+describe('indonesian-news-scrapper', function () {
 
   it('should contains TempoEnglish scrapper', function () {
     assert(!_.isUndefined(index.TempoEnglish), 'TempoEnglish scrapper not found')
