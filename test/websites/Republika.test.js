a6112c8fd0c2da751288d424181464096cd5d72f
ananta pandu <anpandumail@gmail.com>
2017-02-17 14:13:08 +0700

    update republika

--- test/websites/Republika.test.js
+++ test/websites/Republika.test.js
@@ -59,6 +59,23 @@ describe('Republika', function () {
         assert(_.isEqual(result['content'], 'REPUBLIKA.CO.ID, JAKARTA - Kasubdit Kasasi dan Peninjauan Kembali Perdata Khusus Mahkamah Agung Andri Tristianto Sutrisna menjalani pemeriksaan perdana di Gedung KPK, Jakarta, Selasa (23/2). Andi yang ditangkap pada operasi tangkap tangan (OTT) dengan kasus dugaan menerima suap untuk penundaan pengiriman salinan putusan kasasi di MA itu dibawa ke KPK untuk mengkroscek sejumlah barang bukti yang didapatkan dari serangkaian penggeledahan.'))
         assert(_.isEqual(result['source'], 'Republika'))
       })
+      .then(function () {
+        return getContent('test/fixtures/websites/republika/republika-singlepage3.html')
+      })
+      .then(Republika.getDataFromSinglePage)
+      .then(function (result) {
+        assert(result.hasOwnProperty('url'))
+        assert(result.hasOwnProperty('title'))
+        assert(result.hasOwnProperty('date'))
+        assert(result.hasOwnProperty('img'))
+        assert(result.hasOwnProperty('content'))
+        assert(_.isEqual(result['url'], 'http://www.republika.co.id/berita/jurnal-haji/berita-jurnal-haji/17/02/17/oli9fp396-jamaah-haji-ikuti-pemeriksaan-kesehatan-haji'))
+        assert(_.isEqual(result['title'], 'Jamaah Haji Ikuti Pemeriksaan Kesehatan Haji'))
+        assert(_.isEqual(result['date'], 1487312160000))
+        assert(_.isEqual(result['img'], 'http://static.republika.co.id/uploads/images/headline_slide/pemeriksaan-kesehatan-jamaah-calon-haji-kloter-3-mengikuti-pemeriksaan-_160809163333-725.jpg'))
+        assert(_.isEqual(result['content'], 'IHRAM.CO.ID, BANJARMASIH - Kepala Kantor Kementerian Agama Kota Banjarmasin H Sofrayani mengimbau, calon jamaah haji Kota Banjarmasin untuk mengikuti pemeriksaan kesehatan haji tahap pertama. Pemeriksaan ini sebagai bagian tahapan penting yang harus dilalui untuk keberangkatan haji tahun 2017.\"Pemeriksaan kesehatan calon haji tahap pertama sangat penting dilakukan. Ini untuk mengetahui sejauhmana tingkat kesehatan mereka serta upaya penilaian status kesehatan pada seluruh jamaah haji,\" kata H Sofrayani saat meninjau pelaksanaan pemeriksaan kesehatan calon jamaah haji tahap pertama, di Puskesmas Cempaka Kota Banjarmasin, belum lama ini.Dikatakannya, data kesehatan calon jemaah haji yang didapatkan dengan menggunakan metode pemeriksaan medis yang dibakukan sebagai upaya perawatan dan pemeliharaan, serta pembinaan dan perlindungan terhadap calon jamaah haji. Kata dia, berdasarkan kouta tahun 2017, sebanyak 633 calon hamaah haji secara bertahap akan diperiksa kondisi kesehatanya oleh tim pemeriksa kesehatan di Puskesmas yang ditunjuk Kepala Dinas Kesehatan Kota Banjarmasin.Kepala Seksi Haji dan Umrah (Kasi PHU) H Burhan Noor saat mendampingi Kakankemenag dalam pemeriksanaan tersebut mengatakan, pemeriksaaan calon jamaah haji dilaksanakan guna menindak lanjuti Peraturan Menteri Kesehatan Nomor 15 tahun 2016 tentang Istithaah Kesehatan Jemaah Haji. dalam peraturan itu disebutkan jamah haji harus memenuhi syarat istithaah kesehatan haji, memenuhi syarat istithaah kesehatan dengan pendampingan atau tidak memenuhi istithaah kesehatan haji untuk sementara.\"Dalam pemeriksaan kesehatan pada tahap pertama ini meliputi pengambilan sampel darah, pemeriksaan kandungan urine, elektrokradiografi (EKG), radiologi dada dengan jumlah CJH yang diperiksa pada hari ini berjumlah lebih kurang 50 CJH,\" ujar dia.Kegiatan Pemeriksaan Kesehatan Calon Jamaah Haji tahap pertama tersebut direncanakan dilaksanakan sejak tanggal 13 sampai dengan 25 Pebruari 2017 dan diikuti seluruh Calon Jamaah Haji yang berasal dari Kota Banjarmasin.'))
+        assert(_.isEqual(result['source'], 'Republika'))
+      })
   })
 
   // // === this test do I/O
